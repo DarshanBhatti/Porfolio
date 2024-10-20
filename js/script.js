dabbb99bced2a7b890ff7e623fc71c608d7fd4af
DarshanBhatti <169125220+DarshanBhatti@users.noreply.github.com>
2024-10-20 15:25:04 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const menuIcons = document.querySelector("#menu-icons");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcons.onclick = () => {
+    navLinks.classList.toggle('active');
+};
+
+
+const toggleSwitch = document.querySelector('#switch');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
+
+// Apply the current theme on page load
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+// Listen for toggle switch change and apply theme
+toggleSwitch.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+function downloadResume() {
+    const link = document.createElement('a');
+    link.href = '../darshan bhatii.pdf';
+    link.download = 'resume.pdf'; 
+    link.click();
+}
